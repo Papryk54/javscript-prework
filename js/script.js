f15499456184905c27272ad56634e63ac071c974
Patryk Olszewski <patrykolszewski420@gmail.com>
2024-09-06 12:48:31 +0200

    Wynik gry

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ if (randomNumber == 2) {
 if (randomNumber == 3) {
 	computerMove = "nożyce";
 }
-printMessage("ruch komputera to: " + computerMove);
 
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
 
@@ -28,3 +27,44 @@ if (playerInput == "3") {
 }
 
 printMessage("Twój ruch to: " + playerMove);
+printMessage("ruch komputera to: " + computerMove);
+
+if (computerMove == "kamień" && playerMove == "papier") {
+	printMessage("Wygrana!");
+}
+
+if (computerMove == "papier" && playerMove == "nożyce") {
+	printMessage("Wygrana!");
+}
+
+if (computerMove == "nożyce" && playerMove == "kamień") {
+	printMessage("Wygrana!");
+}
+
+if (computerMove == "kamień" && playerMove == "kamień") {
+	printMessage("Remis!");
+}
+
+if (computerMove == "papier" && playerMove == "papier") {
+	printMessage("Remis!");
+}
+
+if (computerMove == "nożyce" && playerMove == "nożyce") {
+	printMessage("Remis!");
+}
+
+if (computerMove == "kamień" && playerMove == "nożyce") {
+	printMessage("Przegrywasz!");
+}
+
+if (computerMove == "papier" && playerMove == "kamień") {
+	printMessage("Przegrywasz!");
+}
+
+if (computerMove == "nożyce" && playerMove == "papier") {
+	printMessage("Przegrywasz!");
+}
+
+if (playerMove == "nieznany ruch") {
+	printMessage("Nie ma takiego ruchu");
+}

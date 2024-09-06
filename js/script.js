b7ea0a128b8661987a3ffe881ffe8ee0f085c5d8
Patryk Olszewski <patrykolszewski420@gmail.com>
2024-09-06 12:29:32 +0200

    OdczytanieRuchuKomputera

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("Wylosowana liczba to: " + randomNumber);
+if (randomNumber == 1) {
+	computerMove = "kamień";
+}
+if (randomNumber == 2) {
+	computerMove = "papier";
+}
+if (randomNumber == 3) {
+	computerMove = "nożyce";
+}
+printMessage(computerMove);
 
-printMessage(
-	computerMove + "! Jeśli Twój ruch to " + playerMove + ", to wygrywasz!"
-);
+// let computerMove = 'nieznany ruch';
+
+// let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+// console.log('Gracz wpisał: ' + playerInput);
+
+// let playerMove = 'nieznany ruch';
+
+// if(playerInput == '1'){
+//   playerMove = 'kamień';
+// }
+
+// printMessage('Twój ruch to: ' + playerMove);
+
+// printMessage(
+// 	computerMove + "! Jeśli Twój ruch to " + playerMove + ", to wygrywasz!"
+// );
+
+// let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+// console.log('Wylosowana liczba to: ' + randomNumber);
+
+// if(randomNumber == 1){
+//   computerMove = 'kamień';
+// }
+
+// printMessage('Mój ruch to: ' + computerMove);
